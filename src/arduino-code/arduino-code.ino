ea881c03b0f81d657c757f5672c1d7fcb5ef40a4
Sashwat K <sashwat0001@gmail.com>
2019-10-24 12:30:09 +0530

    Added WiFi connection

--- src/arduino-code/arduino-code.ino
+++ src/arduino-code/arduino-code.ino
@@ -9,11 +9,17 @@
  * Pins used: Ultrasonic sensor:  Trigger pin - D3
  *                                Echo pin - D2
  */
+#include <ESP8266WiFi.h> // For WiFi
+
+const char *ssid =  "Sashwat's Note 8"; // WiFi SSID
+const char *pass =  "123456789"; // WiFi PASS
 
 int ultrasonicSensorTriggerPin = D3; // Ultrasonic sensor - trigger - digital pin 4
 int ultasonicSensorEchoPin = D2; // Ultrasonic sensor - echo - digital pin 5
 long durationForPulse; // Store duration from sending a pulse to receiving it
 long waterLevelDistance; // Stores the distance between water and sensor in cm
+
+WiFiClient client; // WiFi class object
 
 void setup() {
   // put your setup code here, to run once:
@@ -21,7 +27,18 @@ void setup() {
   pinMode(ultrasonicSensorTriggerPin, OUTPUT);
   pinMode(ultasonicSensorEchoPin, INPUT);
   Serial.begin(9600);
-  Serial.print("Ultrasonic Sensor:-");
+  Serial.println("Waste Management:-");
+  Serial.println("Project by Nandakishore, Ajanya and Sandhya.");
+  delay(10);
+  Serial.print("Connecting to ");Serial.println(ssid);
+  WiFi.begin(ssid, pass); // WiFi connection initialisation
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED) 
+          {
+            delay(500);
+            Serial.print(".");
+          }
+  Serial.println("Connected to WiFi.");
 }
 
 void loop() {
